79e20dbfe8284fccb70a7a3a5e240abc65192979
lfrledh <lfrledh@gmail.com>
2026-02-28 15:59:17 +0800

    Initial Commit

---
+++ D/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  D
+//
+//  Created by lfrledh on 2/28/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ DTests/DTests.swift
@@ -0,0 +1,17 @@
+//
+//  DTests.swift
+//  DTests
+//
+//  Created by lfrledh on 2/28/26.
+//
+
+import Testing
+@testable import D
+
+struct DTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
